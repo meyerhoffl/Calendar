083632f5964a6bf6cb9a81e2fa3889f590d4e7da
meyerhoffl <laura.meyerhoff@gmail.com>
2013-04-26 12:37:12 -0500

    test01

---
+++ test/test_call_integration.rb
@@ -0,0 +1,30 @@
+require 'test/unit'
+require 'calendar.rb'
+
+class CalIntegrationTest < Test::Unit::TestCase
+
+
+def test_1_1
+  month = ARGV[0]
+  year = ARGV[1]
+
+puts `cal #{month} #{year}`
+
+end
+
+
+
+end
+
+
+
+
+  # def test_01_print_month
+  #   month = "January"
+  #   assert_equal(month, 1)
+  # end
+
+  # def test_01_print_year
+  #   year = 1800
+  #   assert_equal(year, 1800)
+  # end
